cf44d3fd359c9193993c9e69c6dfad63bb7b6bc6
Thierry Chappuis <tchappui@gmail.com>
2016-10-24 11:11:32 +0200

    Activité 2: ajout du formulaire sans interactivité

---
+++ activités/activite_2/js/liensweb.js
@@ -0,0 +1,227 @@
+/* 
+ * Activité 1
+ */
+
+/**
+ * Liste des liens Web à afficher. Un lien est défini par :
+ * - son titre
+ * - son URL
+ * - son auteur (la personne qui l'a publié)
+ */
+var listeLiens = [{
+    titre: "So Foot",
+    url: "http://sofoot.com",
+    auteur: "yann.usaille"
+}, {
+    titre: "Guide d'autodéfense numérique",
+    url: "http://guide.boum.org",
+    auteur: "paulochon"
+}, {
+    titre: "L'encyclopédie en ligne Wikipedia",
+    url: "http://Wikipedia.org",
+    auteur: "annie.zette"
+}];
+
+
+/**
+ * Structure de données représentant un élément Lien. 
+ * Convention: les identifiants des propriétés et méthodes privées commencent
+ * par un caractère de soulignement "_".
+ */
+var Lien = {
+
+    /**
+     * Méthode d'initialisation des objets de type Lien.
+     * description: objet contenant les propriétés titre, url et auteur
+     */
+    initLien: function(description) {
+        // Création des nouveaux éléments
+        this._sectionElt = document.createElement("section");
+        this._sectionElt.classList.add("lien");
+
+        this._titreElt = document.createElement("h2");
+        this._aElt = document.createElement("a");
+        this._urlElt = document.createElement("span");
+
+        this._auteurElt = document.createElement("p");
+
+        // Ajout des données
+        this._aElt.textContent = description.titre;
+        this._aElt.href = description.url;
+        this._urlElt.textContent = " " + description.url;
+        this._auteurElt.textContent = "Ajouté par " + description.auteur;
+
+        // Construction de l'arbre
+        this._titreElt.appendChild(this._aElt);
+        this._titreElt.appendChild(this._urlElt);
+
+        this._sectionElt.appendChild(this._titreElt);
+        this._sectionElt.appendChild(this._auteurElt);
+
+        // Application des styles
+        this._appliquerStyles();
+    },
+
+    /**
+     * Méthode publique retournant la racine de l'arbre, c-à-d l'élément section.
+     */
+    get: function() {
+        return this._sectionElt;
+    },
+
+    /**
+     * Méthode privée responsable d'appliquer les styles.
+     */
+    _appliquerStyles: function() {
+        //Styles pour l'élément titre
+        this._titreElt.style.marginTop = "0";
+        this._titreElt.style.marginBottom = "0.5ex";
+
+        // Styles pour l'élément a
+        this._aElt.style.textDecoration = "none";
+        this._aElt.style.color = "#428bca";
+
+        // Styles pour l'élément url
+        this._urlElt.style.fontSize = getComputedStyle(this._auteurElt).fontSize;
+
+        // Styles pour l'élément auteur
+        this._auteurElt.style.marginTop = "0";
+        this._auteurElt.style.marginBottom = "0";
+    },
+};
+
+/**
+ * Structure de donnée représentant la liste de liens dans le DOM.
+ */
+var Liste = {
+
+    initListe: function() {
+        this._contenu = [];
+        this._contenuElt = document.getElementById("contenu");
+    },
+
+    ajouter: function(lien) {
+        this._contenu.push(lien.get());
+        this._contenuElt.appendChild(lien.get());
+    },
+}
+
+
+var Formulaire = {
+    initFormulaire: function() {
+        this._Feedback.init(false);
+        this._Bouton.init(true);
+        this._Formulaire.init(true);
+        this._Feedback.afficher("Ceci est un essai");
+    },
+
+    _Feedback: {
+        init: function(visible) {
+            this._blocElt = document.createElement("p");
+            this._blocElt.style.backgroundColor = "#CEE7F0";
+            if (!visible) {
+                this._blocElt.style.display = "none";
+            }
+
+            var contenuElt = document.getElementById("contenu");
+            document.body.insertBefore(this._blocElt, contenuElt);
+        },
+
+        afficher: function(texte) {
+            this._blocElt.textContent = texte;
+            this._blocElt.style.display = "block";
+            setTimeout(function () {
+                this._blocElt.style.display = "none";
+            }.bind(this), 5000);
+        },
+
+    },
+
+    _Bouton: {
+        init: function(visible) {
+            this._blocElt = document.createElement("div");
+            this._boutonElt = document.createElement("button");
+            this._boutonElt.textContent = "Ajouter un lien";
+            if (!visible) {
+                this._blocElt.style.display = "none";
+            }
+            this._blocElt.appendChild(this._boutonElt);
+
+            var contenuElt = document.getElementById("contenu");
+            document.body.insertBefore(this._blocElt, contenuElt);
+        },
+
+        afficher: function() {},
+
+        cacher: function() {},
+    },
+
+    _Formulaire: {
+        init: function(visible) {
+            this._blocElt = document.createElement("form");
+            this._blocElt.style.margin = "20px 0px";
+
+            this._auteurElt = this._creerSaisieTexte("auteur", "Entrez votre nom", "20%");
+            this._blocElt.appendChild(this._auteurElt);
+
+            this._titreElt = this._creerSaisieTexte("titre", "Entrez le titre du lien", "25%");
+            this._titreElt.style.marginLeft = "10px";
+            this._blocElt.appendChild(this._titreElt);
+
+            this._urlElt = this._creerSaisieTexte("url", "Entrez l'URL du lien", "25%");
+            this._urlElt.style.marginLeft = "10px";
+            this._blocElt.appendChild(this._urlElt);
+
+            this._boutonElt = document.createElement("input");
+            this._boutonElt.setAttribute("type", "submit");
+            this._boutonElt.setAttribute("value", "Ajouter");
+            if (!visible) {
+                this._blocElt.style.display = "none";
+            }
+            this._boutonElt.style.marginLeft = "10px";
+            this._blocElt.appendChild(this._boutonElt);
+
+            var contenuElt = document.getElementById("contenu");
+            document.body.insertBefore(this._blocElt, contenuElt);
+        },
+
+        _creerSaisieTexte: function(name, placeholder, width) {
+            var element = document.createElement("input");
+            element.setAttribute("type", "texte");
+            element.setAttribute("id", name);
+            element.setAttribute("name", name);
+            element.setAttribute("placeholder", placeholder);
+            element.required = true;
+            element.style.width = width;
+            return element;
+        },
+
+        afficher: function() {},
+
+        cacher: function() {},
+    },
+}
+
+
+/**
+ * Fonction d'exécution principale du script. Elle constitue le point d'entrée
+ * pour l'exécution.
+ */
+function main() {
+    // Création et initialisation de la liste
+    var liste = Object.create(Liste);
+    liste.initListe();
+
+    // Itération à travers la liste de lien
+    listeLiens.forEach(function(description) {
+        var lien = Object.create(Lien);
+        lien.initLien(description);
+        liste.ajouter(lien);
+    });
+
+    // Ajout du formulaire
+    var formulaire = Object.create(Formulaire);
+    formulaire.initFormulaire();
+}
+
+main();
